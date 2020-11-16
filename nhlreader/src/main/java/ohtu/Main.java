f689f77ec822b393ea26d091ee5b74b811524075
Tykky <40919314+Tykky@users.noreply.github.com>
2020-11-16 21:50:31 +0200

    kohta 2

--- nhlreader/src/main/java/ohtu/Main.java
+++ nhlreader/src/main/java/ohtu/Main.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+        
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        Arrays.sort(players);
+        for(Player p : players) {
+            if(p.getNationality().equals("FIN")) {
+                System.out.println(p.getName() + " " + p.getTeam() + " " + p.getGoals() + " + " + p.getAssists() + " = " + ((int)p.getGoals() + (int)p.getAssists()));
+            }
+        }
+    }
+  
 }
--- nhlreader/src/main/java/ohtu/Player.java
+++ nhlreader/src/main/java/ohtu/Player.java
@@ -1,20 +1,65 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player> {
     private String name;
+    private String team;
+    private int goals;
+    private int assists;
+    private String nationality;
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public String getTeam() {
+        return this.team;
+    }
+
+    public int getGoals() {
+        return this.goals;
+    }
+
+    public int getAssists() {
+        return this.assists;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    public String getNationality() {
+        return this.nationality;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int compareTo(Player player) {
+        int s1 = this.goals + this.assists;
+        int s2 = player.goals + this.assists;
+        if(s1 > s2) return -1;
+        if(s1 < s2) return 1;
+        return 0;
     }
       
 }
